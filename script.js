1fa58366356d6e95fb9402225ba5d35ec835b30f
Olivier G <zotri@hotmail.com>
2024-06-05 18:00:48 +0200

    wip

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const gameContainer = document.querySelector('#game');
 const player = document.querySelector('#player');
+let lastObstacleTime = new Date;
+const obstacles = [];
+
+// CONTROLS ###########################
 
 const jump = () => {
 	if (player.getAnimations()[0]?.animationName == 'jump') return;
@@ -7,7 +12,60 @@ const jump = () => {
 	setTimeout(() => player.classList = 'run', 600);
 }
 
-// CONTROLS (only jump)
 document.addEventListener('keydown', (event) => {
 	if (['ArrowUp', ' '].includes(event.key)) jump();
 });
+
+// OBSTACLES ##########################
+
+const addObstacle = () => {
+	const obstacle = document.createElement('div');
+	obstacle.classList.add('fox');
+	obstacles.push(obstacle)
+	gameContainer.prepend(obstacle);
+	lastObstacleTime = new Date;
+	setTimeout(() => {
+		obstacle.remove();
+		obstacles.pop();
+	}, 2000);;
+}
+
+// COLLISIONS #########################
+
+const checkCollision = () => {
+	const playerBody = player.getBoundingClientRect();
+	obstacleBody = obstacles[-1].getBoundingClientRect();
+	console.log(playerBody.right < obstacleBody.left || 
+						 playerBody.left > obstacleBody.right || 
+						 playerBody.bottom < obstacleBody.top || 
+						 playerBody.top > obstacleBody.bottom);
+
+	// obstacles.forEach(obstacle => {
+	// 	obstacleBody = obstacle.getBoundingClientRect();
+	// 	console.log(playerBody.right < obstacleBody.left || 
+ //           playerBody.left > obstacleBody.right || 
+ //           playerBody.bottom < obstacleBody.top || 
+ //           playerBody.top > obstacleBody.bottom);
+	// });
+}
+
+// MAIN LOOP ##########################
+
+let obstacleWaiting = false
+
+const mainLoop = () => {
+	const collides = checkCollision();
+
+	const now = new Date;
+	if (obstacleWaiting || now - lastObstacleTime < 500) return;
+
+	obstacleWaiting = true;
+	const randTime = Math.random() * 2000 + 1;
+
+	setTimeout(() => {
+		addObstacle();
+		obstacleWaiting = false;
+	}, randTime);
+}
+
+setInterval(() => mainLoop(), 100);
