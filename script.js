14fa372a24111d9157bae8789d1aba3c65c4c777
Olivier G <zotri@hotmail.com>
2024-06-09 18:46:37 +0200

    ok

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const gameContainer = document.getElementById('game');
 const player = document.getElementById('player');
-const obstacles = [];
 
 // CONTROLS ###########################
 
@@ -15,29 +14,8 @@ document.addEventListener('keydown', (event) => {
 	if (['ArrowUp', ' '].includes(event.key)) jump();
 });
 
-// OBSTACLES ##########################
-
-const addObstacle = () => {
+setInterval(() => {
 	const obstacle = document.createElement('div');
 	obstacle.classList.add('sprite', 'rock');
-	obstacles.push(obstacle)
 	gameContainer.prepend(obstacle);
-}
-
-// MAIN LOOP ##########################
-
-const mainLoop = () => {
-	const firstObstacle = obstacles[0];
-	if (!firstObstacle) return;
-
-	const obstacleBody = firstObstacle.getBoundingClientRect();
-
-	// remove obstacle if out of gameContainer
-	else if (obstacleBody.right < gameContainer.getBoundingClientRect().left) {
-		firstObstacle.remove();
-		obstacles.shift();
-	}
-}
-
-const mainLoopInterval = setInterval(() => mainLoop(), 100);
-const obstacleGenerationInterval = setInterval(() => addObstacle(), 1500);
+}, 1500);
